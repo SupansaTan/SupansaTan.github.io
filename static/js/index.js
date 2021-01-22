69d028668800909511959fdebd9b68389653381f
SupansaTan <s6201012620236@email.kmutnb.ac.th>
2021-01-22 20:03:09 +0700

    Fixed values (Literals) in JavaScript

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,12 @@ function click_img() {
 function addWord() {
     document.getElementById("demo").innerHTML = "&emsp;Paragraph changed.";
 }
+
+/* JS Syntax*/
+// number
+document.getElementById("demo_num1").innerHTML = 10.50;
+document.getElementById("demo_num2").innerHTML = 1001;
+
+// string
+document.getElementById("demo_str1").innerHTML = "John";
+document.getElementById("demo_str2").innerHTML = 'John';
