af4f85dff87f2e96dc8eaf82f843cd1d3bf340df
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2021-01-23 21:09:30 +0700

    Add example of JavaScript Arithmetic and can click arrow to scroll to top

--- static/js/index.js
+++ static/js/index.js
@@ -6,6 +6,25 @@ function addWord() {
     document.getElementById("demo").innerHTML = "&emsp;Paragraph changed.";
 }
 
+/* Scroll to top */
+$(document).ready(function(){
+    // animate arrow
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollTop').fadeIn();
+        } 
+        else {
+            $('.scrollTop').fadeOut();
+        }
+    });
+
+    // click --> scroll to top
+    $('.scrollTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
+
 /* JS Syntax*/
 // number
 document.getElementById("demo_num1").innerHTML = 10.50;
@@ -20,3 +39,26 @@ document.getElementById("js_compute").innerHTML = (7 + 6) * 4;
 
 // expression 
 document.getElementById("js_expression").innerHTML = "John" + " " + "Doe";
+
+/* JS Arithmetic */
+var x = 5, y = 2, char, var_values = "";
+var a = x + y; 
+var b = x - y;
+var c = x * y; 
+var d = x / y; 
+var e = x % y; 
+x++ 
+var f = x; 
+x-- 
+var g = x; 
+h = x **2; 
+i = Math.pow(x,2);
+total = [a,b,c,d,e,f,g,h,i]
+
+// gather the values of each variable 
+for (char=65, i=0; char<=73, i<9; char++ ,i++) {
+    var_values += String.fromCharCode(char).toLowerCase() + " = " + total[i] + "<br>";
+}
+
+// show value of all variables
+document.getElementById("js_arithmetic_exp").innerHTML = var_values;
